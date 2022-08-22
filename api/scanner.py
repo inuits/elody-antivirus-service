d4ef34e7c7686ae8ba0c542c7e8885f2c4800e20
Gijs Vermeulen <gverm@inuits.eu>
2022-08-22 14:16:30 +0200

    Refs #96736 send scan result in virus_detected message

--- api/scanner.py
+++ api/scanner.py
@@ -17,11 +17,12 @@ class Scanner:
     def __get_raw_id(self, item):
         return item["_key"] if "_key" in item else item["_id"]
 
-    def __signal_virus_detected(self, mediafile):
+    def __signal_virus_detected(self, mediafile, scan_result):
         attributes = {"type": "dams.virus_detected", "source": "dams"}
         data = {
             "filename": mediafile["filename"],
             "mediafile_id": self.__get_raw_id(mediafile),
+            "scan_result": scan_result,
         }
         event = CloudEvent(attributes, data)
         message = json.loads(to_json(event))
@@ -33,4 +34,4 @@ class Scanner:
             scan_result = scan.scan_stream(file)
         if scan_result:
             app.logger.error(f'VIRUS DETECTED IN {mediafile["filename"]} {scan_result}')
-            self.__signal_virus_detected(mediafile)
+            self.__signal_virus_detected(mediafile, scan_result)
